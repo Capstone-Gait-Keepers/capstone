c2df78b954498795bb5e86347729a75ebfc8a239
Daniel Raymond <dan@raymond.ch>
2023-10-20 22:45:31 -0400

    Added initial yaml template

--- data_analysis/data_capture.py
+++ data_analysis/data_capture.py
@@ -11,28 +11,56 @@
 
 import serial
 import time
+from json import dumps
+from datetime import datetime
+
+from data_types import Recording, RecordingEnvironment, Event, Source
+
 
 # Name of your serial port (e.g., '/dev/cu.usbserial-0001', or COM5).
-serial_port = '/dev/cu.usbserial-0001'
-
-def collect_data(filename, seconds):
+def collect_data(seconds: float, serial_port = '/dev/cu.usbserial-0001'):
     # Open the serial port for communication with the ESP8266.
     ser = serial.Serial(serial_port, 115200)
-    t_end = time.time() + seconds #data collection will run for this many seconds
-    
+    t_end = time.time() + seconds # Data collection will run for this many seconds
+    vibes = [] # Raw accelerometer data
+    button_presses = [] # Timestamps of button presses
+    start_time = None
+
     # Open a file to save the data (create it if it doesn't exist).
-    with open(filename, 'a') as file:
-        try:
-            while time.time() < t_end:
-                data = ser.readline().decode().strip()  # Read data from the serial port.
-                print(data)  # Print the data to the console.
-                file.write(data + '\n')  # Write the data to the file.
-        except KeyboardInterrupt:
-            # Close the serial port and the file when you interrupt the script.
-            ser.close()
-            file.close()
+    try:
+        while time.time() < t_end:
+            # TODO: Data should include timestamps and button presses. Split data by commas?
+            data = ser.readline().decode().strip()  # Read data from the serial port.
+            print(data) # Print the data to the console.
+            vibes.append(float(data))
+            # if start_time is None:
+            #     start_time = data
+            # if data == 'button':
+            #   button_presses.append(data - start_time)
+    except KeyboardInterrupt:
+        # Close the serial port and the file when you interrupt the script.
+        ser.close()
+    return vibes, button_presses
 
 
-filename = 'datasets/' + input('Input the file name (description of the data): ') + '.csv'
-seconds = input('Input the number of seconds to collect data: ')
-collect_data(filename, int(seconds))
+if __name__ == "__main__":
+    # filename = 'datasets/' + input('Input the file name (description of the data): ') + '.csv'
+    # collect_data(filename, int(seconds))
+
+    # ! Change as needed
+    env = RecordingEnvironment(
+        location='Tals bedroom',
+        fs=100,
+        floor='hardwood',
+        obstacle_radius=0.5,
+        wall_radius=0.5,
+    )
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+    seconds = input('Input the number of seconds to collect data: ')
+    assert input(f'Is this environment correct? \n{dumps(env.to_dict(), sort_keys=True, indent=2)}\n (y/n) ') == 'y', 'Please update the environment in data_capture.py'
+    vibes, button_presses = collect_data(int(seconds))
+
+    events = [Event('step', t, source=Source.USER) for t in button_presses]
+    rec = Recording(env, events, vibes)
+    rec.to_yaml(f'datasets/{timestamp}.yaml')
