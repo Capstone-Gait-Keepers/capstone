d6a9981707391ca0d66055e62e7775ad695ab816
Daniel Raymond <dan@raymond.ch>
2023-11-10 00:35:06 -0500

    Added piezo code

---
+++ fw/piezo_pickup/piezo_pickup.ino
@@ -0,0 +1,23 @@
+const int microphonePin = A0;
+#define ALPHA 0.2
+
+uint16_t output = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+uint16_t EWMA(uint16_t old_output, uint16_t reading) {
+  return ALPHA * ((float)reading) + (1 - ALPHA) * ((float)old_output);
+}
+
+void loop() {
+  uint16_t reading = analogRead(microphonePin);
+  output = EWMA(output, reading);
+  Serial.print(400);
+  Serial.print(" ");
+  Serial.print(output);
+  Serial.print(" ");
+  Serial.println(600);
+}
+
