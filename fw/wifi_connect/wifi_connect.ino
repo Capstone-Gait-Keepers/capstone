76ac29318f38f0d442688fd009920c522c7365d4
Sarah Macintosh <s5macint@uwaterloo.ca>
2023-11-20 19:50:45 -0500

    THEYRE TALKIN, but they are not agreeing

--- fw/wifi_connect/wifi_connect.ino
+++ fw/wifi_connect/wifi_connect.ino
@@ -1,98 +1,85 @@
+/*
+  Complete project details: https://RandomNerdTutorials.com/esp8266-nodemcu-https-requests/ 
+  Based on the BasicHTTPSClient.ino Created on: 20.08.2018 (ESP8266 examples)
+*/
+
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
 
-
-/*ADD YOUR PASSWORD BELOW*/
-const char *ssid = "DataCity-802 - 2.4GHz";
-const char *password = "6340631697";
-
-WiFiClientSecure client;
-HTTPClient http;
-
-/*
-* Connect your controller to WiFi
-*/
-void connectToWiFi() {
-  //Connect to WiFi Network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to WiFi");
-  Serial.println("...");
-  WiFi.begin(ssid, password);
-  int retries = 0;
-  while ((WiFi.status() != WL_CONNECTED) && (retries < 15)) {
-    retries++;
-    delay(500);
-    Serial.print(".");
-  }
-  if (retries > 14) {
-      Serial.println(F("WiFi connection FAILED"));
-  }
-  if (WiFi.status() == WL_CONNECTED) {
-      Serial.println(F("WiFi connected!"));
-      Serial.println("IP address: ");
-      Serial.println(WiFi.localIP());
-  }
-  Serial.println(F("Setup ready"));
-}
-
-//SETUP
+// Replace with your network credentials
+const char* ssid = "DataCity-802 - 2.4GHz";
+const char* password = "6340631697";
 
 void setup() {
-  Serial.begin(9600);
-  connectToWiFi();
+  Serial.begin(115200);
+  //Serial.setDebugOutput(true);
 
-  String URL = "https://capstone-backend-f6qu.onrender.com/api/sensor_metadata"; // Works with HTTP
-  http.begin(client, URL); // Works with HTTP
+  Serial.println();
+  Serial.println();
+  Serial.println();
 
-  /*send a gift to julia*/
-    String postData = "data=hellojulia";
-
-    delay(1000);            // See if this prevents the problm with connection refused and deep sleep
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
-
-    int httpCode = http.POST(postData);   //Send the request
-    String payload = http.getString();    //Get the response payload
-
-//debug
-    if (httpCode == -1) {
-      Serial.print("HTTP return code: -1. Failure to establish a connection or an issue during the HTTP request");
-  // Code to execute if the condition is false
-  } else { 
-    Serial.println(httpCode);   //Print HTTP return code}
+  //Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
   }
-
-    Serial.print(" Payload: ");
-    Serial.println(payload);    //Print request response payload
-
-  http.end();  //Close connection
-
 }
 
 void loop() {
+  // wait for WiFi connection
+  if ((WiFi.status() == WL_CONNECTED)) {
+
+    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+
+    // Ignore SSL certificate validation
+    client->setInsecure();
+    
+    //create an HTTPClient instance
+    HTTPClient https;
+    
+    //Initializing an HTTPS communication using the secure client
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(*client, "https://capstone-backend-f6qu.onrender.com/api/sensor_metadata")) {  // https://capstone-backend-f6qu.onrender.com/api/sensor_metadata
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      //int httpCode = https.GET();
+
+    //send a gift to julia
+    String postData = "data=hellojulia";
+
+    delay(1000);            // See if this prevents the problm with connection refused and deep sleep
+    https.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
+
+    int httpCode = https.POST(postData);   //Send the request
+    String payload = https.getString();    //Get the response payload
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = https.getString();
+          Serial.println(payload);
+        }
+      } else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+
+      https.end();
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
+  }
+  Serial.println();
+  Serial.println("Waiting 2min before the next round...");
+  delay(120000);
 }
-
-/*connect and do things
-IPAddress server(10,0,0,138);
-String PostData = "someDataToPost";
-
-if (client.connect(server, 80)) {
-  client.println("POST /Api/AddParking/3 HTTP/1.1");
-  client.println("Host: 10.0.0.138");
-  client.println("User-Agent: Arduino/1.0");
-  client.println("Connection: close");
-  client.print("Content-Length: ");
-  client.println(PostData.length());
-  client.println();
-  client.println(PostData);
-}
-*/
-
-//
-// Post data
-
-
-  
 
 
 
