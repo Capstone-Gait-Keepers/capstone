835e0f7d833aacf8198472f3df2edb144542d774
Daniel Raymond <dan@raymond.ch>
2024-02-22 23:46:38 -0500

    Added env var to deployment

--- frontend/src/backend_interface.ts
+++ frontend/src/backend_interface.ts
@@ -2,7 +2,7 @@ export async function queryBackend(endpoint: String): Promise<any> {
     const url = import.meta.env.VITE_BACKEND_URL;
     console.log("url: ", url);
     if (!url) {
-        throw new Error("BACKEND_URL not defined. Please define it in .env file.");
+        throw new Error("VITE_BACKEND_URL not defined. Please define it in .env file.");
     }
   const response = await fetch(url + endpoint);
     return response.json();
