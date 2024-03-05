9834f92ff2df65326775913cb5efe332a1033908
Daniel Raymond <dan@raymond.ch>
2024-03-04 23:49:11 -0500

    Added plotting

---
+++ frontend/src/components/Plot.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="plot" />
+</template>
+
+<script setup>
+import Plotly from 'plotly.js-dist';
+import { defineProps, onMounted } from 'vue';
+
+
+const { x, y, xlabel, ylabel } = defineProps({
+  x: Array,
+  y: Array,
+  xlabel: String,
+  ylabel: String,
+});
+
+
+onMounted(() => {
+  const data = [{x, y, type: 'scatter'}];
+  const layout = {
+    // title: 'A Fancy Plot',
+    margin: {l: 32, r: 0, t: 0, b: 32}, // Remove margin
+    // paper_bgcolor: "#fff", // Transparent background
+    // plot_bgcolor: "#fff", // Transparent plot area
+    xaxis: {
+      title: {
+        text: xlabel,
+        font: {
+          size: 12,
+          color: '#7f7f7f'
+        }
+      }
+    },
+    yaxis: {
+      title: {
+        text: ylabel,
+        font: {
+          size: 12,
+          color: '#7f7f7f'
+        }
+      }
+    }
+  };
+  const config = {displayModeBar: false};
+  Plotly.newPlot('plot', data, layout, config);
+});
+</script>
--- frontend/src/views/DetailView.vue
+++ frontend/src/views/DetailView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import BasePage from '@/components/BasePage.vue'
+import Plot from '@/components/Plot.vue'
 </script>
 
 <template>
   <BasePage>
     <h1>Learn More</h1>
+    <p>Here's a fancy plot:</p>
+    <Plot :x="[2, 3, 4]" :y="[2, 3, 2]" xlabel="X" ylabel="Y" />
   </BasePage>
 </template>
