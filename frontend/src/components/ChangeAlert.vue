6a4169a5eb1665d166d3f200beca5089d780c665
Daniel Raymond <dan@raymond.ch>
2024-03-12 00:35:27 -0400

    Started insights page

---
+++ frontend/src/components/ChangeAlert.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="alert-box">
+    <p class="exclamation">!</p>
+    <div>
+      <strong>Changes detected!</strong>
+      <p>Negative changes were detected in your Balance over the last month. Consider setting an appointment up with your doctor to check in on your balance.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.alert-box {
+  padding: 0.5rem 1rem;
+  color: black;
+  border: 2px solid var(--yellow);
+  border-radius: 8px;
+  margin: 1rem;
+  gap: 1rem;
+  display: flex;
+}
+
+.exclamation {
+  font-size: 3rem;
+  color: var(--yellow);
+  line-height: 1;
+}
+
+strong {
+  font-weight: bold;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,116 @@
 <script setup lang="ts">
 import BasePage from '@/components/BasePage.vue'
+import ChangeAlert from '@/components/ChangeAlert.vue';
 </script>
 
 <template>
   <BasePage>
-    <h1>Individual Insights</h1>
+    <h1>Good Afternoon Marjorie</h1>
+    <ChangeAlert />
+    <div class="insights-container">
+      <h2>Welcome to your personal insights</h2>
+
+      <h3>Balance Metrics</h3>
+      <div class="metrics-container">
+        <div>
+          <p>WalkWise measures two metrics associated with balance.</p>
+          <ul>
+            <li>Stride Time Coefficient of Variation</li>
+            <li>Stride Time Gait Asymmetry</li>
+          </ul>
+        </div>
+        <div class="status-indicator">
+          <p>This Month</p>
+          <img src="@/assets/icon-thumbs-up.svg" />
+          <p>2 of 4 metrics showed positive changes compared with last month!</p>
+        </div>
+        <div class="status-indicator">
+          <p>This Year</p>
+          <img src="@/assets/icon-thumbs-up.svg" />
+          <p>2 of 4 metrics showed positive changes compared with last year!</p>
+        </div>
+      </div>
+
+      <h3>Neurodegenerative Metrics</h3>
+      <div class="metrics-container">
+        <div>
+          <p>WalkWise measures two metrics associated with Neurodegenerative diseases.</p>
+          <ul>
+            <li>Stride Time Phase Synchronization</li>
+            <li>Stride Time Conditional Entropy</li>
+            <li>Stride Time Gait Asymmetry</li>
+          </ul>
+        </div>
+        <div class="status-indicator">
+          <p>This Month</p>
+          <div class="flat-bar"></div>
+          <p>No changes in neurodegenerative related metrics this month.</p>
+        </div>
+        <div class="status-indicator">
+          <p>This Year</p>
+          <img src="@/assets/icon-warning.svg" />
+          <p>There were negative changes in 2 of the 3 metrics related to neurodegenerative disease. View the <a href="#">Breakdown</a> to see more.</p>
+        </div>
+      </div>
+
+      <h3>Dementia Metrics</h3>
+      <div class="metrics-container">
+        <div>
+          <p>WalkWise measures 4 metrics associated with Dementia.</p>
+          <ul>
+            <li>Stride Time</li>
+            <li>Cadence</li>
+            <li>Stride Time Coefficient of Variation</li>
+            <li>Swing Time Gait Asymmetry</li>
+          </ul>
+        </div>
+        <div class="status-indicator">
+          <p>This Month</p>
+          <div class="flat-bar"></div>
+          <p>No changes in Dementia related metrics this month.</p>
+        </div>
+        <div class="status-indicator">
+          <p>This Year</p>
+          <div class="flat-bar"></div>
+          <p>No changes in Dementia related metrics compared with last year.</p>
+        </div>
+      </div>
+    </div>
   </BasePage>
 </template>
+
+<style scoped>
+.insights-container {
+  padding: 1rem 6rem;
+}
+
+.metrics-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 20px;
+  margin-bottom: 40px;
+}
+
+h3 {
+  font-weight: bold;
+}
+
+.flat-bar {
+  border-bottom: 4px solid var(--color-main);
+  width: 80px;
+  height: 0;
+  margin: 1rem 0;
+}
+
+.status-indicator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+
+.status-indicator img {
+  width: 50px;
+  height: 50px;
+}
+</style>
