18a70fc980021625cf5c0610a500f3b3a937c507
Daniel Raymond <draymond63@users.noreply.github.com>
2024-04-29 18:06:51 +0000

    data and changes from demo

---
+++ backend/static/assets/NotFoundView-DN8tG1WA.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c as n,b as a,t as r}from"./index-dq3P82ji.js";const c={};function s(e,l){return t(),n("main",null,[a("h1",null,"404, page not found: "+r(e.$route.fullPath),1)])}const f=o(c,[["render",s]]);export{f as default};
